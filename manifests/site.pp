a3860994acf34e322b437e7fda164efb0193949c
Tray Torrance <torrancew@gmail.com>
2018-10-01 23:31:11 -0700

    Use role fact as a pseudo node classifier

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,9 @@
 include profile::base
+
+$facts.dig('role').then |$role| {
+  if defined("role::${role}") {
+    include "role::${role}"
+  } else {
+    warning("Could not find class for role ${role}")
+  }
+}
